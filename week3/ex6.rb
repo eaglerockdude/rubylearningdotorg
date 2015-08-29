c35dc08b9ace3286b04a831e921d6c731ee0b431
ken mcfadden <kendmcfadden@gmail.com>
2015-08-29 16:55:42 -0700

    added ex6 grandma

---
+++ week3/ex6.rb
@@ -0,0 +1,36 @@
+# Exercise6. Write a Deaf Grandma program.
+# Whatever you say to grandma (whatever you type in), she should respond with HUH?!
+#  SPEAK UP, SONNY!, unless you shout it (type in all capitals).
+# If you shout, she can hear you (or at least she thinks so)
+# and yells back, NO, NOT SINCE 1938!
+# To make your program really believable, have grandma shout a
+# different year each time; maybe any year at random between 1930 and 1950.
+# You can't stop talking to grandma until you shout BYE.
+
+def random_year
+  a = (1930...1951).to_a
+  a[rand(a.size)]
+end
+
+talk = true
+
+while talk
+
+  puts  "You are now speaking with Grandma.. say BYE to hangup.."
+  speak  = $stdin.gets.chomp
+  if speak == 'BYE'
+  puts "Nice speaking with you Sonny!"
+   talk = false
+  else
+
+    if  /\A[[:upper:]]*\Z/   =~ speak
+      puts "NO, NOT SINCE #{random_year}"
+    else
+      puts "HUH? I CANNOT HERE YOU SONNY-BOY!."
+    end
+
+  end
+
+end
+
+
