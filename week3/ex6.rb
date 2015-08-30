f46c483e768bf7f80760624ec3d7e73987c35de3
ken mcfadden <kendmcfadden@gmail.com>
2015-08-30 07:17:49 -0700

    simplfied grandma program

--- week3/ex6.rb
+++ week3/ex6.rb
@@ -8,25 +8,20 @@
 # You can't stop talking to grandma until you shout BYE.
 
 talk = true
-
+a = (1930...1951).to_a
 while talk
-
   puts  "You are now speaking with Grandma.. say BYE to hangup.."
   speak  = $stdin.gets.chomp
   if speak == 'BYE'
-  puts "Nice speaking with you Sonny!"
-   talk = false
+    puts "Nice speaking with you Sonny!"
+    talk = false
   else
-
     if  /\A[[:upper:]]*\Z/   =~ speak
-      a = (1930...1951).to_a
       puts "NO, NOT SINCE #{a[rand(a.size)]}"
     else
       puts "HUH? I CANNOT HERE YOU SONNY-BOY!."
     end
-
   end
-
 end
 
 
