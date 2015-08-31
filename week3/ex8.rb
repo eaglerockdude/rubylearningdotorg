7fffe757fb1bfd4777c3499b9d6848f5c813be41
ken mcfadden <kendmcfadden@gmail.com>
2015-08-31 12:56:13 -0700

    fizz buzz exercise

---
+++ week3/ex8.rb
@@ -0,0 +1,19 @@
+# ken mcfadden rubylearning.net
+
+# Write a Ruby program that prints the numbers from 1 to 100.
+#  But for multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz".
+#  For numbers which are multiples of both three and five print "FizzBuzz".
+
+(1..100).each do   |n|
+
+  if n%3 == 0
+   puts "Fizz"
+  else
+    if n%5 ==  0
+    puts "Buzz"
+    else
+      puts "#{n}"
+    end
+    end
+
+  end  # end do
