8f2af8b6dc5ea77e0675e9f6ccdc441db3e9698c
ken mcfadden <kendmcfadden@gmail.com>
2015-08-31 18:49:34 -0700

    wk3 ex 15

---
+++ week3/ex15.rb
@@ -0,0 +1,11 @@
+# ken mcfadden rubylearning.net wk 3
+
+# Select all correct outputs for the following program:
+
+for i, j in [[1, 2], [3, 4], [5, 6]]
+  p [i, j]
+end
+# Correct answer is :
+# [1, 2]
+# [3, 4]
+# [5, 6]
