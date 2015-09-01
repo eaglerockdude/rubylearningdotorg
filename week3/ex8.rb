b0f9091daa7b43a7e57b08e2e499ac44a48b7160
ken mcfadden <kendmcfadden@gmail.com>
2015-08-31 17:41:04 -0700

    correct ex8

--- week3/ex8.rb
+++ week3/ex8.rb
@@ -4,16 +4,14 @@
 #  But for multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz".
 #  For numbers which are multiples of both three and five print "FizzBuzz".
 
-(1..100).each do   |n|
-
-  if n%3 == 0
-   puts "Fizz"
+(1..100).each do |n|
+  if n % 3 == 0 && n % 5 == 0
+    puts 'FizzBuzz'
+  elsif n%3 == 0
+    puts 'Fizz'
+  elsif n % 5 == 0
+    puts 'Buzz'
   else
-    if n%5 ==  0
-    puts "Buzz"
-    else
-      puts "#{n}"
-    end
-    end
-
-  end  # end do
+    puts "#{n}"
+  end
+end # end do
