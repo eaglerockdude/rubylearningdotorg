20147d69ff1cdb6c6172b8fca8501abab7557e65
ken mcfadden <kendmcfadden@gmail.com>
2015-08-31 18:17:44 -0700

    wk3 ex13

---
+++ week3/ex13.rb
@@ -0,0 +1,13 @@
+# ken mcfadden rubylearning.net week 3
+# rewrite the following code in one line.
+
+# The long way
+# record = Hash.new
+# record[:name] = “Satish”
+# record[:email] = “mail@satishtalim.com”
+# record[:phone] = “919371006659”
+
+# shorter
+record = {name: 'Satish', email: 'mail@satishtalim.com', phone: '919371006659'}
+
+puts "dude"
