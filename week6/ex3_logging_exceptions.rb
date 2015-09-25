a095008ffd98ea3b98aafea2d21d1eb145291c2e
Victor Goff <keeperotphones@gmail.com>
2015-09-25 12:39:20 -0400

    Review of code

--- week6/ex3_logging_exceptions.rb
+++ week6/ex3_logging_exceptions.rb
@@ -45,43 +45,41 @@ require 'logger'
 @exclaim_ary  = []
 @length = 0
 @sentence_tot = 0.0
-@all_txt  = ''
+@all_txt = ''
 
 # begin/end over entire code group for exception handling
 begin
 
   $LOG = Logger.new($stderr)
 
-  $LOG.info("Getting ready to process our text file.")
+  $LOG.info('Getting ready to process our text file.')
 
-  File.open("text.txt") do |f|
-
+  File.open('text.txt') do |f|
     f.each do |record|
-
-      @lines += 1
+      @lines += 1 # if this is an array, eventually, then we should be able to say @lines.size for this value
       @all_txt << record
-      @lines_ary <<  record.lines  # lines to array
+      @lines_ary << record.lines # lines to array -- See above note on lines...
 
       #  puts "#{@lines} #{record}"
 
       # sum total characters
-      @length = record.length
-      @characters +=  @length
+      @length = record.length # This gets reset each time.  Do we keep it somewhere?  Are we interested in what the length of the last record was?
+      @characters += @length # @characters += record.length is as simple and expressive as it should be.
 
       # total words
       rec_words = record.split(' ')
-      @words = rec_words.size
+      @words = rec_words.size # This is also inside an iterator, so gets reset each time.
+      # @words if collected throughout the life if the iteration, can later be counted as @words.count one time, rather than each time.
       @word_total += @words
 
       # total characters exclude spaces
       rec_words.each do |elem|
         chars = elem.size
-        @charactersexcludingspaces += chars
+        @charactersexcludingspaces += chars # Everywhere else you seem to use name_separated_with_spaces... but not here...
       end
+    end # end read loop
 
-    end   # end read loop
-
-    $LOG.info("Read loop complete..printing statistics....")
+    $LOG.info('Read loop complete..printing statistics....')
 
     # count total sentences  & paragraphs
     @period_ary = @all_txt.scan(/\./)
@@ -89,36 +87,38 @@ begin
     @exclaim_ary = @all_txt.scan(/\!/)
     @paragraphs  = @all_txt.scan(/\r\n\r\n/)
 
-    @sentence_tot =  @period_ary.size + @exclaim_ary.size + @question_ary.size
+    @sentence_tot = @period_ary.size + @exclaim_ary.size + @question_ary.size
     # Average words/sentence
-    @avg_words_sent = 0.0
-    @avg_words_sent = @word_total/@sentence_tot
-    @avg_sent_paragraph = 0.0
-    @avg_sent_paragraph =  @sentence_tot / @paragraphs.size
+    @avg_words_sent = 0.0 # you initialize it to 0
+    @avg_words_sent = @word_total / @sentence_tot # Then immediately throw away the value and re-write it.and you are doing Integer math... 
+    @avg_sent_paragraph = 0.0 # Same thing here...
+    @avg_sent_paragraph = @sentence_tot / @paragraphs.size
 
-    puts "------------------------------- Analysis Report -----------------------------------------------------"
-    puts "Number of lines :  #{@lines}"
-    puts "Number of characters  :  #{@characters}"
-    puts "Number of characters excluding spaces   :  #{@charactersexcludingspaces}"
-    puts "Number of words   :  #{@word_total}"
-    puts "Number of sentences ending with a period  '.' is : #{@period_ary.size}"
-    puts "Number of sentences ending with '?'  is : #{@question_ary.size}"
-    puts "Number of sentences ending with '!' is : #{@exclaim_ary.size}"
-    puts "Number of sentences  is : #{@period_ary.size + @exclaim_ary.size + @question_ary.size}"
-    puts "Number of sentences  is : #{@sentence_tot}"
-    puts "Number of paragraphs is  : #{@paragraphs.size}"
-    puts "Avg. number of words per sentence  : #{@avg_words_sent}"
-    puts "Avg. number of sentences per paragraph  : #{@avg_sent_paragraph}"
+    # Averse to Heredoc?  How about??? Why does this work?
+    puts [
+      '------------------------------- Analysis Report -----------------------------------------------------',
+      "Number of lines :  #{@lines}",
+      "Number of characters  :  #{@characters}",
+      "Number of characters excluding spaces   :  #{@charactersexcludingspaces}",
+      "Number of words   :  #{@word_total}",
+      "Number of sentences ending with a period  '.' is : #{@period_ary.size}",
+      "Number of sentences ending with '?'  is : #{@question_ary.size}",
+      "Number of sentences ending with '!' is : #{@exclaim_ary.size}",
+      "Number of sentences  is : #{@period_ary.size + @exclaim_ary.size + @question_ary.size}",
+      "Number of sentences  is : #{@sentence_tot}",
+      "Number of paragraphs is  : #{@paragraphs.size}",
+      "Avg. number of words per sentence  : #{@avg_words_sent}",
+      "Avg. number of sentences per paragraph  : #{@avg_sent_paragraph}"
+    ]
 
-    $LOG.info("Text Analyzer Completed......No errors.")
-
+    $LOG.info('Text Analyzer Completed......No errors.')
   end
-# exception handler:
+  # exception handler:
 rescue Exception => msg
   # display the system generated error message
 
-  puts"The exception handler has been invoked. Here is the error message:"
+  puts 'The exception handler has been invoked. Here is the error message:'
   puts msg.message
   puts msg.backtrace.inspect
 
-end     # begin end for exception handling.
+end # begin end for exception handling.
